f8bf640464eeb3482dccf47bf280163bf4014332
Yui San ae2f <mincraft20046@gmail.com>
2024-11-03 19:28:47 +0900

    Project Name Edit

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "ae2f::BmpCL", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
